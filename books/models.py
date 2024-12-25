b8a6d332506d38614e0c824acf8020cf79539452
AndreiVed <catblues87@gmail.com>
2024-12-25 13:18:43 +0200

    created Book App, Book model, admin panel and made migrations

--- books/models.py
+++ books/models.py
@@ -1,3 +1,26 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    COVER_CHOICES = {
+        "SOFT": "soft",
+        "HARD": "hard"
+    }
+
+    title = models.CharField(max_length=63)
+    author = models.CharField(max_length=63)
+    cover = models.CharField(
+        choices=COVER_CHOICES,
+        max_length=4,
+    )
+    inventory = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)],
+)
+    daily_fee = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator("0.01")],
+    )
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}"
